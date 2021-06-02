ec3bc686dc2dc630659986d02c951128a1fa3a1f
zarok13 <erron13m@gmail.com>
2021-06-02 22:19:08 +0400

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8d75d9fac676adaf4ddacc2f509784a37564946b',
+    'name' => 'zarok13/sswriter',
+  ),
+  'versions' => 
+  array (
+    'zarok13/sswriter' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8d75d9fac676adaf4ddacc2f509784a37564946b',
+    ),
+  ),
+);
